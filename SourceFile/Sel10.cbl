aea06734a49b5f24c49805e9bef6e4a133fe48a1
Yuan <allegoyuanpaolo@gmail.com>
2025-11-13 18:41:47 +0800

    Finished Selection (FIX SEL8!!)

--- SourceFile/Sel10.cbl
+++ SourceFile/Sel10.cbl
@@ -2,11 +2,22 @@
        program-id. SEL10.
 
        environment division.
+           input-output section.
+               file-control.
+                   select outputFile
+                       assign to "Sel10.dat"
+                       organization is line sequential
+                       file status is OFStat.
 
        data division.
+           file section.
+               fd outputFile.
+                   01 bufferLine pic x(64).
        local-storage section.
        
            01 letter pic x.
+           01 OFStat pic xx.
+           01 result pic x(32).
 
        procedure division.
        
@@ -16,12 +27,36 @@
 
            if  letter = 'A' or letter = 'E' OR letter = 'O'             -
             OR letter = 'I' OR letter ='U'
-               Display "Your letter is a vowel"
+               move "Your letter is a vowel" to result
            else if letter = 'a' or letter = 'e' OR letter =             -
            'o' OR letter = 'i' OR letter = 'u'
-               display "Your letter is a vowell"
+               move "Your letter is a vowel" to result
            else 
-               display "Your letter is a consonant"
+               move  "Your letter is a consonant" to result
            end-if.
+
+           display result
+
+               open input outputFile
+                   if OFStat = "00"
+                       close outputFile 
+                       open extend outputFile
+                   else
+                       open output outputFile
+                   end-if.
+                   
+                   move spaces to bufferLine
+                   string "Letter: " delimited by size
+                          letter delimited by size
+                          into bufferLine
+                   end-string
+                       write bufferLine
+
+                       move spaces to bufferLine
+                   write bufferLine from result
+                   write bufferLine from "-----------------------"
+                   write bufferLine from spaces
+
+               close outputFile
 
        goback. 
